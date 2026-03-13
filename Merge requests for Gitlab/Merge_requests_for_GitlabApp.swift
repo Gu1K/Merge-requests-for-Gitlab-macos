09184387446f22c7904566de08c0a93a77a34ef1
gu1k <gmougenot01@gmail.com>
2026-03-13 11:17:55 +0100

    size

--- Merge requests for Gitlab/Merge_requests_for_GitlabApp.swift
+++ Merge requests for Gitlab/Merge_requests_for_GitlabApp.swift
@@ -2,18 +2,16 @@ import SwiftUI
 
 @main
 struct Merge_requests_for_GitlabApp: App {
-    // Stocke le token de manière sécurisée dans les préférences de l'utilisateur
     @AppStorage("gitlabToken") private var apiToken: String = ""
     
     var body: some Scene {
-        // Crée l'icône dans la barre de menu
+        // Fenêtre confortable : 500x600
         MenuBarExtra("GitLab MR", systemImage: "tray.and.arrow.down.fill") {
             ContentView()
-                .frame(width: 320, height: 450)
+                .frame(width: 500, height: 1000)
         }
         .menuBarExtraStyle(.window)
         
-        // Ajoute la fenêtre de réglages standard macOS (Cmd + ,)
         Settings {
             SettingsView()
         }
