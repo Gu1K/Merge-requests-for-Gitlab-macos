048b0810a9e8c049216cfd1c2bb56047cba5611b
Gu1K <gmougenot01@gmail.com>
2026-03-13 10:42:27 +0100

    Initial Commit

---
+++ Merge requests for Gitlab/Merge_requests_for_GitlabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Merge_requests_for_GitlabApp.swift
+//  Merge requests for Gitlab
+//
+//  Created by Guillaume Mougenot on 13/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Merge_requests_for_GitlabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
